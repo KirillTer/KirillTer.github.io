2822605ca7e3171ab12dda7ce14dddb5cb2ffdc6
KirillTer <v-kirillt@microsoft.com>
2021-04-13 17:18:15 +0300

    initial

---
+++ precache-manifest.f4e5b4ab1f512f430b087e67ecd7d86a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89e4035de8c48a9d4635e6f4a4d7ab19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5f52433c1ee5845268a",
+    "url": "/static/css/2.624e4bf4.chunk.css"
+  },
+  {
+    "revision": "c5cb259ea19247479bdf",
+    "url": "/static/css/main.f9570a30.chunk.css"
+  },
+  {
+    "revision": "a5f52433c1ee5845268a",
+    "url": "/static/js/2.6035955c.chunk.js"
+  },
+  {
+    "revision": "53bcbc18f296aa9c77145e9bd080dd5e",
+    "url": "/static/js/2.6035955c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c5cb259ea19247479bdf",
+    "url": "/static/js/main.d2b24c60.chunk.js"
+  },
+  {
+    "revision": "6a737fbb891d42710038",
+    "url": "/static/js/runtime-main.86cec644.js"
+  }
+]);
